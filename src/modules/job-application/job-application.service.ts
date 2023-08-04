79ecfdf444635574c827d16d9dea8e5fcdcddeb4
vaibhav patil <vaibhavpatil@opticalarc.com>
2023-08-04 09:46:11 +0530

    schema registerd

--- src/modules/job-application/job-application.service.ts
+++ src/modules/job-application/job-application.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { JobApplicationDto } from '../../constants/dto/job-application.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { JobApplication } from 'src/schema/job-application/job-application.schema';
 
 @Injectable()
 export class JobApplicationService {
+  constructor(
+    @InjectModel('JobApplication') private JobApplicationModel: JobApplication,
+  ) {}
+
   create(JobApplicationDto: JobApplicationDto) {
     return 'This action adds a new jobApplication';
   }
