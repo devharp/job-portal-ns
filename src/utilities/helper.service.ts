d89196b0ca42608793c3d6f82f06c38262ab011c
vaibhav patil <vaibhavpatil@opticalarc.com>
2023-08-16 12:55:57 +0530

    date format of date filter for post history has changed

--- src/utilities/helper.service.ts
+++ src/utilities/helper.service.ts
@@ -1,26 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class Helper {
-  public async ToYearMonthDayFormat(inputDate: string): Promise<string> {
-    const monthMap = {
-      Jan: '01',
-      Feb: '02',
-      Mar: '03',
-      Apr: '04',
-      May: '05',
-      Jun: '06',
-      Jul: '07',
-      Aug: '08',
-      Sep: '09',
-      Oct: '10',
-      Nov: '11',
-      Dec: '12',
-    };
-    const dateParts = inputDate.split('-');
-    const year = dateParts[0];
-    const month = monthMap[dateParts[1]];
-    const day = dateParts[2];
-    return `${year}-${month}-${day}`;
-  }
-}
+export class Helper {}
